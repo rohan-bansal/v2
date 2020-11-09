47b2b94001574d5c7a2a35c0a1d636de1cf58211
Rohan Bansal <bansalrohan18@gmail.com>
2020-11-09 12:58:03 -0800

    add gradient name and description

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// $(document).ready(function() {
+//     var width = document.getElementById("name-div").style.width;
+//     document.getElementById("intro-div").style.width = width;
+// });
+
+new TypeIt('#type-text', {
+    speed: 100
+})
+.type("Developer. ")
+.pause(500)
+.type("Linux Enthusiast. ")
+.pause(500)
+.type("Guitarist.")
+
+.options({loop: false, lifeLike: true})
+.go();
