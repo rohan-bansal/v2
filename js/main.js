710c01f0a37c6889b88b6480f96cc44fe050263c
Rohan Bansal <bansalrohan18@gmail.com>
2020-11-25 19:06:08 -0800

    sliders

--- js/main.js
+++ js/main.js
@@ -15,3 +15,31 @@ new TypeIt('#type-text', {
 
 .options({loop: false, lifeLike: true})
 .go();
+
+
+
+$('.work-exp').slick({
+    centerMode: true,
+    centerPadding: '60px',
+    slidesToShow: 3,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 3
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
