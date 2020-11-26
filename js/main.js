44638cfe0651911a9e6bc2f3a7dbc48cc5f935c0
Rohan Bansal <bansalrohan18@gmail.com>
2020-11-25 21:39:24 -0800

    add projects and blog

--- js/main.js
+++ js/main.js
@@ -20,10 +20,12 @@ new TypeIt('#type-text', {
 
 $('.work-exp').slick({
     dots: true,
-    infinite: false,
+    infinite: true,
     speed: 300,
     slidesToShow: 3,
     slidesToScroll: 3,
+    autoplay: true,
+    autoplaySpeed: 7000,
     arrows: false,
     // prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
     // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
