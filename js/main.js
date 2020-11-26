2b012cf62008c7f9d917a3ba07083d1048020997
Rohan Bansal <bansalrohan18@gmail.com>
2020-11-25 20:36:52 -0800

    add styling

--- js/main.js
+++ js/main.js
@@ -19,27 +19,40 @@ new TypeIt('#type-text', {
 
 
 $('.work-exp').slick({
-    centerMode: true,
-    centerPadding: '60px',
+    dots: true,
+    infinite: false,
+    speed: 300,
     slidesToShow: 3,
+    slidesToScroll: 3,
+    arrows: false,
+    // prevArrow:"<button type='button' class='slick-prev pull-left'><i class='fa fa-angle-left' aria-hidden='true'></i></button>",
+    // nextArrow:"<button type='button' class='slick-next pull-right'><i class='fa fa-angle-right' aria-hidden='true'></i></button>",
     responsive: [
       {
-        breakpoint: 768,
+        breakpoint: 1024,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 3
+          slidesToShow: 3,
+          slidesToScroll: 3,
+          infinite: true,
+          dots: true
+        }
+      },
+      {
+        breakpoint: 600,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
         }
       },
       {
         breakpoint: 480,
         settings: {
-          arrows: false,
-          centerMode: true,
-          centerPadding: '40px',
-          slidesToShow: 1
+          slidesToShow: 1,
+          slidesToScroll: 1
         }
       }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
     ]
   });
