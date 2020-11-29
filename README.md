d718ae6f5ee01ab2b67770bca0daa6bc3257711b
Rohan Bansal <bansalrohan18@gmail.com>
2020-11-28 16:46:39 -0800

    optimize

---
+++ README.md
@@ -0,0 +1,2 @@
+# Rohan Bansal
+
--- js/main.js
+++ js/main.js
@@ -58,3 +58,112 @@ $('.work-exp').slick({
       // instead of a settings object
     ]
   });
+
+  particlesJS("background-canvas", {
+    "particles": {
+      "number": {
+        "value": 12,
+        "density": {
+          "enable": true,
+          "value_area": 800
+        }
+      },
+      "color": {
+        "value": "#5073b8"
+      },
+      "shape": {
+        "type": "circle",
+        "stroke": {
+          "width": 0,
+          "color": "#5073b8"
+        },
+        "polygon": {
+          "nb_sides": 5
+        },
+      },
+      "opacity": {
+        "value": 0.8,
+        "random": false,
+        "anim": {
+          "enable": true,
+          "speed": 1,
+          "opacity_min": 0.1,
+          "sync": false
+        }
+      },
+      "size": {
+        "value": 15,
+        "random": true,
+        "anim": {
+          "enable": true,
+          "speed": 5,
+          "size_min": 1,
+          "sync": false
+        }
+      },
+      "line_linked": {
+        "enable": false,
+        "distance": 150,
+        "color": "#5073b8",
+        "opacity": 0.4,
+        "width": 1
+      },
+      "move": {
+        "enable": true,
+        "speed": 2,
+        "direction": "none",
+        "random": false,
+        "straight": false,
+        "out_mode": "out",
+        "bounce": false,
+        "attract": {
+          "enable": false,
+          "rotateX": 600,
+          "rotateY": 1200
+        }
+      }
+    },
+    "interactivity": {
+      "detect_on": "canvas",
+      "events": {
+        "onhover": {
+          "enable": true,
+          "mode": "bubble"
+        },
+        "onclick": {
+          "enable": true,
+          "mode": "push"
+        },
+        "resize": true
+      },
+      "modes": {
+        "grab": {
+          "distance": 140,
+          "line_linked": {
+            "opacity": 1
+          }
+        },
+        "bubble": {
+          "distance": 400,
+          "size": 40,
+          "duration": 2,
+          "opacity": 8,
+          "speed": 3
+        },
+        "repulse": {
+          "distance": 200,
+          "duration": 0.4
+        },
+        "push": {
+          "particles_nb": 4
+        },
+        "remove": {
+          "particles_nb": 2
+        }
+      }
+    },
+    "retina_detect": true
+  });
+
+  // document.getElementById("background-canvas").style.minHeight = document.body.scrollHeight + "px";
+  // document.getElementById("background-canvas").style.minWidth = "100vw";
